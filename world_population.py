157a6db81e079f204e1df71ef5215529510882c1
Thomas Erickson <terickson@spotx.tv>
2018-08-14 08:05:38 -0600

    made a world map

--- world_population.py
+++ world_population.py
@@ -1,10 +1,12 @@
 import json
+import pygal
 from country_codes import get_country_code
 
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
+cc_populations = {}
 for pop_dict in pop_data:
     if(pop_dict['Year'] == '2010'):
         try:
@@ -15,6 +17,12 @@ for pop_dict in pop_data:
             print('weird number')
         else:
             if(code):
-                print(code + ': ' + str(country_pop))
+                cc_populations[code] = country_pop
             else:
                 print('error: ' + country_name)
+
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010'
+wm.add('North America', cc_populations)
+
+wm.render_to_file('world_pop.svg')
