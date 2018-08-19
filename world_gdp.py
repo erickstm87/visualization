961ca8297bd004c0a86427273a67987e2fa3595d
erickstm87 <erickstm87@github.com>
2018-08-19 13:22:02 -0600

    finished gdp part

--- world_gdp.py
+++ world_gdp.py
@@ -5,9 +5,37 @@ from country_codes import get_country_code
 
 filename = 'gdp_json.json'
 with open(filename) as f:
-    pop_data = json.load(f)
+    gdp_data = json.load(f)
 
 gdp_countries = {};
-for pop_gdp in pop_data:
+for pop_gdp in gdp_data:
     if(pop_gdp['Year'] == 2016):
-        print(pop_gdp)
+        try:
+            c_name = pop_gdp['Country Name']
+            c_code = get_country_code(c_name)
+            c_gdp = int(float(pop_gdp['Value']))
+        except:
+            print('couldn\'t find')
+        else:
+            if(c_code):
+                gdp_countries[c_code] = c_gdp
+            else:
+                print('no code')
+
+gdp_1, gdp_2, gdp_3 = {}, {}, {}
+temp = []
+for cc, gdp in gdp_countries.items():
+    temp.append(gdp)
+    if(gdp > 10000000000):
+        gdp_1[cc] = gdp
+    elif(gdp < 10000000000):
+        gdp_2[cc] = gdp
+    else:
+        gdp_3[cc] = gdp
+print(min(temp))
+wm = pygal.maps.world.World()
+wm.title = ('gdp in 2016')
+wm.add('0-10b', gdp_2)
+wm.add('>10b', gdp_1)
+wm.add('10b', gdp_3)
+wm.render_to_file('world_gdp.svg')
--- world_population.py
+++ world_population.py
@@ -26,20 +26,21 @@ for pop_dict in pop_data:
 
 cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
 for cc, pop in cc_populations.items():
-    if pop < 10000000:
-        cc_pops_1[cc] = pop
-    elif pop > 10000000:
-        cc_pops_2[cc] = pop
-    else:
-        cc_pops_3[cc] = pop
+      print(pop)
+#     if pop < 10000000:
+#         cc_pops_1[cc] = pop
+#     elif pop > 10000000:
+#         cc_pops_2[cc] = pop
+#     else:
+#         cc_pops_3[cc] = pop
 
-print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+# print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
 
-wm_style = RotateStyle('#336699', base_style=LightColorizedStyle)
-wm = pygal.maps.world.World(style=wm_style)
-wm.title = 'World Population in 2010'
-wm.add('0-10m', cc_pops_1)
-wm.add('10m-1bn', cc_pops_2)
-wm.add('>1bn', cc_pops_3)
+# wm_style = RotateStyle('#336699', base_style=LightColorizedStyle)
+# wm = pygal.maps.world.World(style=wm_style)
+# wm.title = 'World Population in 2010'
+# wm.add('0-10m', cc_pops_1)
+# wm.add('10m-1bn', cc_pops_2)
+# wm.add('>1bn', cc_pops_3)
 
-wm.render_to_file('world_pop.svg')
+# wm.render_to_file('world_pop.svg')
